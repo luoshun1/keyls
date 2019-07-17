b5abe5bf22a93329ef68c35dd4b0f87be22ea190
luoshun <1823202729@qq.com>
2019-07-17 13:10:57 +0800

    ...

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-def findMinAndMax(L):
-    if L != []:
-        min = max = L[0]
-        for number in L:
-            if min > number:
-                min = number
-            if max < number:
-                max = number
-        return (min, max)
-    else:
-        return (None, None)
-
-if findMinAndMax([]) != (None, None):
-    print('测试失败')
-elif findMinAndMax([7]) != (7, 7):
-    print('测试失败')
-elif findMinAndMax([7, 1]) != (1, 7):
-    print('测试失败')
-elif findMinAndMax([7, 1, 9, 4, 5]) != (1, 9):
-    print('测试失败')
-else:
-    print('测试成功')
-
-
+it = iter([1, 2, 3, 4, 5])
+while True:
+    try:
+        x = next(it)
+        print("x = ", x)
+    except StopIteration:
+        break
